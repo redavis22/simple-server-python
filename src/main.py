924d601db7771ddc231fed460a1642c2eba683ab
redavis22 <redavis22@gmail.com>
2016-04-27 18:07:26 -0400

    another message

--- src/main.py
+++ src/main.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Kevin Baskin!'
-@app.route('/clgi')
-    return 'go to clgi.org'
+    return 'Ok now let us get the stock ticker working!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
