0e3d2eafb90e463bf79541a471c9f6ccb37f0136
redavis22 <redavis22@gmail.com>
2016-04-27 18:00:33 -0400

    commit

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello Kevin Baskin!'
-
+@app.route('/clgi')
+    return 'go to clgi.org'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
